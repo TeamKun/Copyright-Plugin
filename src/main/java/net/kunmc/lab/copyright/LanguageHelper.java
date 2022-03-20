6e5f1aacb483fcd0bcf8139c959adccb8c947987
Kei-7777 <kei.logic2252@gmail.com>
2022-03-20 16:03:34 +0900

    add eng jpn

--- src/main/java/net/kunmc/lab/copyright/LanguageHelper.java
+++ src/main/java/net/kunmc/lab/copyright/LanguageHelper.java
@@ -5,28 +5,39 @@ import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Locale;
+import java.util.Properties;
+import java.util.ResourceBundle;
 
 public enum LanguageHelper {
 
-    ENGLISH("English", null),
-    JAPANESE("日本語", "japanese.yml");
+    ENGLISH("English", "english.properties"),
+    JAPANESE("日本語", "japanese.properties");
 
     private String langName;
     private String fileName;
+    private Properties prop;
 
     LanguageHelper(String langName, String fileName) {
         this.fileName = fileName;
         this.langName = langName;
+        this.prop = new Properties();
+        try {
+            prop.load(new FileInputStream(new File(System.getProperty("user.dir") + File.separator + "lang", langName)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public String byMaterial(Material material) {
         if (fileName == null) {
             return material.name();
         } else {
-            Configuration config = YamlConfiguration.loadConfiguration(new File(System.getProperty("user.dir") + File.separator + "lang", fileName));
-            if (config.contains(material.toString().toLowerCase(Locale.ROOT))) {
-                return config.getString(material.toString().toLowerCase(Locale.ROOT));
+            String key = material.getTranslationKey();
+            if (prop.contains(key)) {
+                return prop.getProperty(key);
             } else {
                 return material.name();
             }
